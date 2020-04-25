00976ea3a6899416f98e0d3ec0f2f5d0924ce1b8
Ravinder Saluja <ravz.saluja@yahoo.com>
2020-04-25 23:50:43 +0530

    Initialize cnn

---
+++ cnn.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Apr 25 23:46:01 2020
+
+@author: ravindersaluja
+"""
+
+# Importing Libs
+from keras.models import Sequential
+from keras.layers import Conv2D
+from keras.layers import MaxPooling2D
+from keras.layers import Flatten
+from keras.layers import Dense
+
+classfier = Sequential()
+
