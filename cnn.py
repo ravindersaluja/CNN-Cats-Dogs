01da189f2d8b938ace95850add5c8325ccb2bf81
Ravinder Saluja <ravz.saluja@yahoo.com>
2020-05-01 01:36:50 +0530

    Add train and test data and fit to classfier

--- cnn.py
+++ cnn.py
@@ -12,6 +12,7 @@ from keras.layers import Conv2D
 from keras.layers import MaxPooling2D
 from keras.layers import Flatten
 from keras.layers import Dense
+from keras.preprocessing.image import ImageDataGenerator
 
 # Initializing
 classifier = Sequential()
@@ -40,12 +41,41 @@ classifier.add(Dense(units=1, activation = 'sigmoid'))
 # Compiling CNN
 classifier.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
 
+# Image augmentation - image preprocessing to avoid overfitting-rotates/flips etc the image and creates more batches
+# of it if the dataset is too small and thus reducing overfitting
+# Using ImageDataGenerator class for the same.
 
+with open('pathFile.txt', 'r') as f:
+    l = f.read().splitlines()
+    trainingPath = l[0]+'training_set'
+    testingPath = l[0]+'test_set'
 
+train_datagen = ImageDataGenerator(
+        rescale=1./255,
+        shear_range=0.2,
+        zoom_range=0.2,
+        horizontal_flip=True)
 
+test_datagen = ImageDataGenerator(rescale=1./255)
 
+training_set = train_datagen.flow_from_directory(
+        trainingPath,
+        target_size=(64, 64),
+        batch_size=32,
+        class_mode='binary')
 
+test_set = test_datagen.flow_from_directory(
+    testingPath,
+    target_size=(64, 64),
+    batch_size=32,
+    class_mode='binary')
 
+classifier.fit_generator(
+        training_set,
+        steps_per_epoch=8000,
+        epochs=25,
+        validation_data=test_set,
+        validation_steps=2000)
 
 
 
