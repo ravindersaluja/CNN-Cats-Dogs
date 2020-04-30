5544d8205bee15abd836ef19bcdfe94efb50c0ae
Ravinder Saluja <ravz.saluja@yahoo.com>
2020-04-30 23:08:17 +0530

    add pooling layer

--- cnn.py
+++ cnn.py
@@ -13,7 +13,15 @@ from keras.layers import MaxPooling2D
 from keras.layers import Flatten
 from keras.layers import Dense
 
-classfier = Sequential()
+# Initializing
+classifier = Sequential()
+
+
+# Adding the Convolutional layer and Activation fn
 # input_shape (Reducing the shape because using CPU and not GPU, so (64,64,3), 3 being the channels - RGB)
 # Channels last because using tensorflow backend.
-classifier.add(Conv2D(filters=32, kernel_size=(3,3), input_shape=(64,64,3)), activation='relu')
+classifier.add(Conv2D(filters=32, kernel_size=(3,3), input_shape=(64,64,3), activation='relu'))
+
+# Adding the Pooling layer
+# Adding the max pooling layer to decrease the number of nodes in the future fully connected layers
+classfier.add(MaxPooling2D(pool_size=(2,2)))
